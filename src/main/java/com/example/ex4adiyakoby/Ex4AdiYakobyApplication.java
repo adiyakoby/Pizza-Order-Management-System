3f4862cb907c8395075650eb4935f10aa50ebe37
adi65 <adija@edu.hac.ac.il>
2024-06-08 10:43:38 +0300

    init project

---
+++ src/main/java/com/example/ex4adiyakoby/Ex4AdiYakobyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ex4adiyakoby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex4AdiYakobyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex4AdiYakobyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ex4adiyakoby/Ex4AdiYakobyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ex4adiyakoby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex4AdiYakobyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
