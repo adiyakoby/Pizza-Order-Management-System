ac8b1c9d2058df27ea8edb4ed1c28c233fd7361e
adi65 <adija@edu.hac.ac.il>
2024-06-09 23:14:10 +0300

    Changed position and name of main sprint app

--- src/main/java/hac/SpringRestApplication.java
+++ src/main/java/hac/SpringRestApplication.java
@@ -1,13 +1,13 @@
-package com.example.ex4adiyakoby;
+package hac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ex4AdiYakobyApplication {
+public class SpringRestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Ex4AdiYakobyApplication.class, args);
+        SpringApplication.run(SpringRestApplication.class, args);
     }
 
 }
