4537775fb213c28ca5f935977a6db97d13a8500d
adi65 <adija@edu.hac.ac.il>
2024-06-09 23:13:33 +0300

    added Order.java

---
+++ src/main/java/hac/Orders/Order.java
@@ -0,0 +1,23 @@
+package hac.Orders;
+
+import java.util.ArrayList;
+
+public class Order {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String number;
+    private ArrayList<Pizza> pizzas;
+
+    public Order() {
+    }
+
+    public Order(Long id, String firstName, String lastName, String address, String number) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.number = number;
+    }
+}
