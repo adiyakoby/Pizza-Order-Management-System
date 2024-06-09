279b7b5aad3a651bda03cb9dbe3cddd0e6416b67
adi65 <adija@edu.hac.ac.il>
2024-06-09 23:13:57 +0300

    added Pizza.java

---
+++ src/main/java/hac/Orders/Pizza.java
@@ -0,0 +1,14 @@
+package hac.Orders;
+
+import java.util.ArrayList;
+
+public class Pizza {
+    private ArrayList<String> ingredients;
+
+    public Pizza() {
+
+    }
+    public Pizza(ArrayList<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+}
