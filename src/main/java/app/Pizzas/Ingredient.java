7ce2233e8880d37b3aee87bd9e6c270212f43577
adi65 <adija@edu.hac.ac.il>
2024-06-10 16:09:53 +0300

    updated ingredient to represent one Ingredient

---
+++ src/main/java/app/Pizzas/Ingredient.java
@@ -0,0 +1,18 @@
+package app.Pizzas;
+
+
+public class Ingredient {
+    private String name;
+    private double price;
+    private String image;
+
+    public Ingredient(String name, double price, String image) {
+        this.name = name;
+        this.price = price;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
