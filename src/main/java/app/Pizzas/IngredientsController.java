06d19ee3055bde7bc7c7b51789569f55b1879124
adi65 <adija@edu.hac.ac.il>
2024-06-10 16:10:01 +0300

    added IngredientsController.java

---
+++ src/main/java/app/Pizzas/IngredientsController.java
@@ -0,0 +1,37 @@
+package app.Pizzas;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class IngredientsController {
+    private static final ArrayList<Ingredient> ingredients = new ArrayList<>(List.of(
+            new Ingredient("mozzarella", 10, "."),
+            new Ingredient("Tomatoes", 10, "."),
+            new Ingredient("Mushrooms", 10, "."),
+            new Ingredient("Green Peppers", 10, "."),
+            new Ingredient("Pepperoni", 10, "."),
+            new Ingredient("Green Olives", 10, "."),
+            new Ingredient("Black Olives", 10, "."),
+            new Ingredient("Extra Cheese", 10, ".")
+        ));
+
+    public IngredientsController() {
+    }
+
+    public ArrayList<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public Ingredient getIngredient(String name) {
+        for (Ingredient ingredient : ingredients) {
+            if (name.equalsIgnoreCase(ingredient.getName())) {
+                return ingredient;
+            }
+        }
+        return null;
+    }
+
+
+}
