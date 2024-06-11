7de8842ae2756d6cbc62b6adc26d6cb41263ecd7
adi65 <adija@edu.hac.ac.il>
2024-06-11 13:33:53 +0300

    added Home component

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,9 +1,28 @@
+import {Button, Col, Container, Row} from "react-bootstrap";
 
 
 function Home() {
     return (
-        <div>hello</div>
-    )
+        <Container fluid className="vh-100 d-flex flex-column justify-content-center align-items-center text-center bg-light">
+            <Row>
+                <Col>
+                    <h1>Welcome to Pizza Order App</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <div className="button-group mt-4">
+                        <Button variant="primary" className="home-button mx-2">
+                            Start new Order
+                        </Button>
+                        <Button variant="secondary" className="home-button mx-2">
+                            Check order
+                        </Button>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default Home;
