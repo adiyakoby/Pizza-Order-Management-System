3280f94417352b917cc5387908f344b36017c9e0
adi65 <adija@edu.hac.ac.il>
2024-06-12 17:33:51 +0300

    added CartContext.js

---
+++ frontend/src/Components/Context/CartContext.js
@@ -0,0 +1,33 @@
+import {createContext, useReducer} from "react";
+
+export const CartContextProvider = createContext();
+
+
+function cartReducer(state, action) {
+    switch(action.type) {
+        case "add" : {
+            return [
+                ...state, action.payload
+            ]
+        }
+
+        case "remove" : {
+            return state.filter((pizza, i) => pizza !== action.payload)
+        }
+    }
+
+}
+
+
+function CartContext({children}) {
+    const [cart, dispatch] = useReducer(cartReducer, [])
+
+    return(
+        <CartContextProvider value={{cart, dispatch}}>
+            {children}
+        </CartContextProvider>
+
+    );
+}
+
+export default CartContext;
