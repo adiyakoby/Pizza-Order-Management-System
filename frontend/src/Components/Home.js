85fdb4d27853757a39d48aafd579f32a46521b07
adi65 <adija@edu.hac.ac.il>
2024-06-13 16:50:51 +0300

    improved appearance

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,27 +1,23 @@
-import {Col, Container, Row} from "react-bootstrap";
-import {Link} from "react-router-dom";
-
+import React from "react";
+import { Col, Container, Row, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Home() {
-
-
-
-
     return (
         <Container fluid className="vh-100 d-flex flex-column justify-content-center align-items-center text-center bg-light">
-            <Row>
+            <Row className="mb-4">
                 <Col>
-                    <h1>Welcome to Pizza Order App</h1>
+                    <h1 className="display-4">Welcome to Pizza Order App</h1>
                 </Col>
             </Row>
             <Row>
                 <Col className="button-group mt-4">
-                        <Link to="/Order" className="btn btn-primary mx-2">
-                            Start new Order
-                        </Link>
-                        <Link to="/CheckOrder" className="btn btn-secondary mx-2">
-                            Check Order
-                        </Link>
+                    <Button as={Link} to="/Order" variant="primary" size="lg" className="mx-2">
+                        Start New Order
+                    </Button>
+                    <Button as={Link} to="/CheckOrder" variant="secondary" size="lg" className="mx-2">
+                        Check Order
+                    </Button>
                 </Col>
             </Row>
         </Container>
